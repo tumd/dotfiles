16d78b789a482c6a48120388dea8d2bce378ea70
Thomas Danielsson <thomas@elajt.se>
2010-12-29 16:23:13 +0100

    prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -80,11 +80,11 @@ colors
 host_prompt_color() {
   case ${SSH_CLIENT} in 
     [0-9]*)
-      echo "@%m"
+      echo "@%m:"
     ;;;
     
     *)
-      echo ""
+      echo ":"
     ;;;
   esac
 }
@@ -104,7 +104,7 @@ ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[red]%}?"
 #export PROMPT=$'$(host_prompt_color)%n@%m:%~$(git_prompt_info)$ %{$fg[white]%}'
 if [ "$(whoami)" = "root" ]; then NCOLOR="red"; else NCOLOR="white"; fi
 
-PROMPT='%{$fg[$NCOLOR]%}%n%{$reset_color%}$(host_prompt_color):%{$reset_color%}%{$fg[green]%}%c %(!.#.$)%{$reset_color%} '
+PROMPT='%{$fg[$NCOLOR]%}%n%{$reset_color%}$(host_prompt_color)%{$fg[green]%}%c %(!.#.$)%{$reset_color%} '
 RPROMPT='$(git_prompt_info)'
 #RPROMPT='[%*]'
 
