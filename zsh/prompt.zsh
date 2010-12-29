227fbeda254c2dcac1074b4599b5ecd0290c41b3
Thomas Danielsson <thomas@elajt.se>
2010-12-29 16:08:02 +0100

    Changed the prompt layout a bit when in a ssh session.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -80,11 +80,11 @@ colors
 host_prompt_color() {
   case ${SSH_CLIENT} in 
     [0-9]*)
-      echo "%{$fg[red]%}"
+      echo "@%m"
     ;;;
     
     *)
-      echo "%{$fg[green]%}"
+      echo ""
     ;;;
   esac
 }
@@ -104,7 +104,7 @@ ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[red]%}?"
 #export PROMPT=$'$(host_prompt_color)%n@%m:%~$(git_prompt_info)$ %{$fg[white]%}'
 if [ "$(whoami)" = "root" ]; then NCOLOR="red"; else NCOLOR="white"; fi
 
-PROMPT='%{$fg[$NCOLOR]%}%n:%{$reset_color%}%{$fg[green]%}%c%{$reset_color%} $(host_prompt_color)%(!.#.$)%{$reset_color%} '
+PROMPT='%{$fg[$NCOLOR]%}%n$(host_prompt_color):%{$reset_color%}%{$fg[green]%}%c %(!.#.$)%{$reset_color%} '
 RPROMPT='$(git_prompt_info)'
 #RPROMPT='[%*]'
 
