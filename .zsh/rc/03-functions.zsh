93f72d015b8497e835d11c328c36184caf8769a3
Thomas Danielsson <thomas@elajt.se>
2020-07-21 17:54:37 +0200

    Added cd-helper from grml

--- .zsh/rc/03-functions.zsh
+++ .zsh/rc/03-functions.zsh
@@ -1,2 +1,14 @@
 function mkcd() { [[ $# == 1 ]] && mkdir -p -- "$1" && cd -- "$1" }
 compdef _directories mkcd
+
+# https://github.com/grml/grml-etc-core/blob/master/etc/zsh/zshrc#L3393-L3402
+# smart cd function, allows switching to /etc when running 'cd /etc/fstab'
+function cd () {
+    if (( ${#argv} == 1 )) && [[ -f ${1} ]]; then
+        [[ ! -e ${1:h} ]] && return 1
+        print "Correcting ${1} to ${1:h}"
+        builtin cd ${1:h}
+    else
+        builtin cd "$@"
+    fi
+}
