6e49fce194c3f63eed7feba7dd232e4d319390a6
Thomas Danielsson <thomas@elajt.se>
2021-02-24 08:37:49 +0100

    Add manpages for brewed curl

--- .zsh/rc/curl.zsh
+++ .zsh/rc/curl.zsh
@@ -1,3 +1,4 @@
 # Add curl from homebrew to path if it exists
 [[ -d /usr/local/opt/curl/bin ]] && path=(/usr/local/opt/curl/bin $path)
 [[ -d /usr/local/opt/curl/share/zsh/site-functions ]] && fpath=(/usr/local/opt/curl/share/zsh/site-functions $fpath)
+[[ -d /usr/local/opt/curl/share/man ]] && manpath=(/usr/local/opt/curl/share/man $manpath)
